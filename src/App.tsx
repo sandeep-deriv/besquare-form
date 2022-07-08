4513b098bef7ed520a452740ca141387abe5a00e
Sandeep <sandeep@deriv.com>
2022-07-08 18:15:32 +0800

    added styling and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import "./style.css";
+import "./App.css";
 
 export default function App() {
   const [name, setName] = React.useState<string>("");
-  const [age, setAge] = React.useState<string | undefined>();
+  const [age, setAge] = React.useState<string | undefined>("");
+  const [color, setColor] = React.useState<string | undefined>("red");
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -13,25 +14,59 @@ export default function App() {
     setAge(event.target.value);
   };
 
+  const changeColor = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setColor(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(name, age, color);
+  };
+
   return (
-    <div>
-      <Input name="Name" value={name} onChange={changeName} />
-      <Input name="Age" value={age} onChange={changeAge} />
+    <div className="pa-16">
+      <form onSubmit={handleSubmit}>
+        <Input name="Name" value={name} onChange={changeName} />
+        <Input name="Age" value={age} onChange={changeAge} />
+        <Select name="Favourite Color" value={color} onChange={changeColor} />
+        <button type="submit" className="btn-primary mb-16">
+          Submit
+        </button>
+        <button className="btn-secondary">Clear</button>
+      </form>
     </div>
   );
 }
 
 type InputType = {
   name: string;
-  value: string;
+  value: string | undefined;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ value, onChange, name }) => {
+const Input = ({ value, onChange, name }: InputType) => {
   return (
-    <div>
+    <div className="mb-16">
       <label>{name}</label>
       <input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+type SelectType = {
+  name: string;
+  value: string | undefined;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const Select = ({ value, onChange, name }: SelectType) => {
+  return (
+    <div className="mb-16">
+      <label>{name}</label>
+      <select value={value} onChange={onChange}>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+      </select>
     </div>
   );
 };
